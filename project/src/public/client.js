173a01adc47dd97a0955126d8d5b751e73cb9d9a
hienlpv <hienlpv@gmail.com>
2024-04-13 15:33:51 +0700

    mars dashboard

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,7 +1,10 @@
 let store = {
     user: { name: "Student" },
-    apod: '',
+    photos: [],
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+    activeRover: 'Curiosity',
+    roverInfo: {},
+    loading: false
 }
 
 // add our markup to the page
@@ -10,96 +13,148 @@ const root = document.getElementById('root')
 const updateStore = (store, newState) => {
     store = Object.assign(store, newState)
     render(root, store)
+
 }
 
 const render = async (root, state) => {
     root.innerHTML = App(state)
 }
 
+const renderRoverTab = (activeRover) => (html, rover) => html += Tab(rover, rover === activeRover)
+
+const renderPhotoCarousel = (html, photo, index) => html += Photo(photo.img_src, index === 0)
 
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
+    return `
+        ${Header()}
+        ${Main(state)}
+    `
+}
+
+const Header = () => {
+    return `
+        <header class="container my-4">
+            <h1>Mars Dashboard</h1>
+        </header>
+    `
+}
+
+const Main = (state) => {
+    let { loading } = state
 
     return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
+        <main class="container">
+            ${Menu(state)}
+            ${loading ? Spinner() : Slider(state)}
         </main>
-        <footer></footer>
+    `
+}
+
+const Menu = (state) => {
+    let { rovers, activeRover } = state
+
+    return `
+        <ul class="nav nav-tabs">
+            ${rovers.reduce(renderRoverTab(activeRover), '')}
+        </ul>
+    `
+}
+
+const Tab = (rover, active) => {
+    return `
+        <li class="nav-item">
+            <a class="nav-link ${active ? 'active' : ''}" href="#" onclick="onClickTab('${rover}')">${rover}</a>
+        </li>
+    `
+}
+
+const Slider = (state) => {
+    let { roverInfo, photos } = state
+
+    return `
+        <div class="card h-100 mt-5">
+            <div id="carousel" class="carousel slide">
+                <div class="carousel-inner">
+                    ${photos.reduce(renderPhotoCarousel, '')}
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            ${RoverInfo(roverInfo)}
+        </div>
+    `
+}
+
+const RoverInfo = (rover) => {
+    return `
+        <div class="card-body">
+            <h3 class="card-title mb-3">${rover.name}</h3>
+            <p class="card-text text-capitalize"><b>Status</b>: ${rover.status}</p>
+            <p class="card-text"><b>Launch Date</b>: ${rover.launch_date}</p>
+            <p class="card-text"><b>Landing Date</b>: ${rover.landing_date}</p>
+        </div>
+    `
+}
+
+const Photo = (src, active) => {
+    return `
+        <div class="carousel-item bg-dark text-center ${active ? 'active' : ''}" style="height: 50vh">
+            <img src="${src}" style="height: 100%" />
+        </div>
+    `
+}
+
+const Spinner = () => {
+    return `
+        <div class="mt-5 d-flex justify-content-center align-items-center">
+            <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
     `
 }
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
     render(root, store)
+    getActiveRoverInfo(store)
+
 })
 
-// ------------------------------------------------------  COMPONENTS
+// ------------------------------------------------------  Events
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
-
-    return `
-        <h1>Hello!</h1>
-    `
+const onClickTab = (rover) => {
+    let storeMap = Immutable.Map({ ...store });
+    storeMap = storeMap.set('activeRover', rover);
+    updateStore(store, { activeRover: storeMap.get('activeRover') })
+    getActiveRoverInfo(store)
 }
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
+const loading = (load) => {
+    let storeMap = Immutable.Map({ ...store });
+    storeMap = storeMap.set('loading', load);
+    updateStore(store, { loading: storeMap.get('loading') })
 }
 
 // ------------------------------------------------------  API CALLS
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+// API call
+const getActiveRoverInfo = (state) => {
+    let { activeRover } = state
 
-    fetch(`http://localhost:3000/apod`)
+    loading(true);
+
+    fetch(`http://localhost:3000/rover/${activeRover}`)
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
-
-    return data
+        .then(res => {
+            loading(false)
+            updateStore(store, { ...res })
+        })
 }
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -3,26 +3,25 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
+var cors = require('cors')
 
 const app = express()
 const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
-
-// example API call
-app.get('/apod', async (req, res) => {
+app.get('/rover/:rover', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
+        let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.rover}/photos?sol=10&api_key=${process.env.API_KEY}`
+        let { photos } = await fetch(url).then(res => res.json())
+        res.send({ photos, roverInfo: photos[0].rover })
     } catch (err) {
         console.log('error:', err);
     }
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
